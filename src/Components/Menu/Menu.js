5f27803b7a64d1f0c4ee3a8a6e4521d115b2ba0c
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-05-28 11:29:54 -0700

    Adds icecream menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,5 +1,56 @@
 import React from 'react';
 
+const icecream = {
+	chocolate: {
+		desciption: ''
+	},
+	vanilla: {
+		desciption: ''
+	},
+	bunnyTracks: {
+		desciption: ''
+	},
+	strawberry: {
+		desciption: ''
+	},
+	rockyRoad: {
+		desciption: ''
+	},
+	caramelCheesecake: {
+		desciption: ''
+	},
+	chocolateChipCookieDough: {
+		desciption: ''
+	},
+	sugarFreeVanilla: {
+		desciption: ''
+	},
+	butterPecan: {
+		desciption: ''
+	},
+	birthdayCake: {
+		desciption: ''
+	},
+	bubblegum: {
+		desciption: ''
+	},
+	cottonCandy: {
+		desciption: ''
+	},
+	blackCherry: {
+		desciption: ''
+	},
+	mintChocolateChip: {
+		desciption: ''
+	},
+	cookiesAndCream: {
+		desciption: ''
+	},
+	orangeSherbet: {
+		desciption: ''
+	},
+}
+
 const Menu = (props) => {
 	return <h1>Menu</h1>
 }
